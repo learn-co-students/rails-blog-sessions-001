8468757cc6832561132023dedabbbbc0a73fcc02
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-22 14:34:57 -0400

    solution

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
 
   def new
     @comment = Comment.new
-  end
-
-  def edit
   end
 
   def create
@@ -18,26 +15,6 @@ class CommentsController < ApplicationController
         format.html { render action: 'new' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment.post, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 RailsBlog::Application.routes.draw do
+
+  root 'posts#index'
+
+  get 'signup' => 'users#new', :as => 'signup'
+  get 'login' => 'sessions#new', :as => 'login'
+  delete 'logout' => 'sessions#destroy', :as => 'logout'
+
+  resources :sessions
   resources :users
   resources :tags
   
