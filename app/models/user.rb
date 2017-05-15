bc79dc6910c9133b9e4b04fef95ef857ff3bd2e9
epang1 <epang1@gmail.com>
2017-05-15 13:51:40 -0400

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
-  validates_uniqueness_of :name
+  validates_uniqueness_of :email
+  validates_uniqueness_of :email
   validates_presence_of :name
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 RailsBlog::Application.routes.draw do
   resources :users
+
   resources :tags
   
   resources :posts do 
     resources :comments
   end
+
+  get '/', to: 'posts#index', as: :main_page
+  get '/sessions', to: 'sessions#main_page'
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#authenticate'
+  get '/logout', to: 'sessions#logout'
+
+  get '/signup', to: 'users#new'
+
 
 end
